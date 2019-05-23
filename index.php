b06b5879031d2963f8fdeea542de10db93c0ebbd
wenrivera <wenrivera@gmail.com>
2019-05-23 18:19:19 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
     
 <html>
  <head>
-  <title>Prueba 1 de GitHub</title>
+  <title>Prueba 2 de GitHub</title>
  </head>
  <body>
- <?php echo '<p>Hola Mundo</p>'; ?>
+ <?php echo '<p>Hola Mundo, ¿como estás?</p>'; ?>
  </body>
 </html>
